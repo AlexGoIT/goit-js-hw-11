dba2765a3083ff1ae68458ffda3d3a1b95efc00b
AlexGoIT <alexandrpuryaev@gmail.com>
2023-04-20 11:52:54 +0300

    Add infinite scroll

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 
 import { getImages } from './js/getImages';
 import { createImageCardMarkup } from './js/createImageCardMarkup';
@@ -8,15 +9,19 @@ import { createImageCardMarkup } from './js/createImageCardMarkup';
 const PER_PAGE = 40;
 let searchQuery = '';
 let pageCount = 1;
+let isLoading = false;
+let totalPages = 0;
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('.load-more'),
+  loadMoreAuto: document.querySelector('.load-more-auto'),
 };
 
 refs.searchForm.addEventListener('submit', onSubmit);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+refs.loadMoreAuto.classList.add('is-hidden');
+window.addEventListener('scroll', debounce(onScroll, 300));
 
 const lightBox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
@@ -26,7 +31,6 @@ const lightBox = new SimpleLightbox('.gallery a', {
 function onSubmit(e) {
   e.preventDefault();
 
-  refs.loadMoreBtn.classList.add('is-hidden');
   searchQuery = e.target.searchQuery.value.trim();
 
   if (!searchQuery) {
@@ -43,6 +47,8 @@ function onSubmit(e) {
 }
 
 async function renderUI() {
+  isLoading = true;
+
   try {
     const response = await getImages(searchQuery, pageCount);
 
@@ -55,20 +61,14 @@ async function renderUI() {
       return;
     } else if (pageCount === 1) {
       Notify.success(`Hooray! We found ${totalHits} images.`);
+      totalPages = Math.ceil(totalHits / PER_PAGE);
     }
 
     // Створюємо розмітку карток з результату пошуку
     refs.gallery.insertAdjacentHTML('beforeend', createImageCardMarkup(hits));
-
-    if (pageCount * PER_PAGE < totalHits) {
-      refs.loadMoreBtn.classList.remove('is-hidden');
-      scroll();
-    } else {
-      refs.loadMoreBtn.classList.add('is-hidden');
-      Notify.failure(
-        "We're sorry, but you've reached the end of search results."
-      );
-    }
+    refs.loadMoreAuto.classList.add('is-hidden');
+    isLoading = false;
+    scroll();
 
     lightBox.refresh();
   } catch (error) {
@@ -79,7 +79,7 @@ async function renderUI() {
 
 function onLoadMore() {
   pageCount++;
-
+  refs.loadMoreAuto.classList.remove('is-hidden');
   renderUI();
 }
 
@@ -93,7 +93,22 @@ function scroll() {
     .firstElementChild.getBoundingClientRect();
 
   window.scrollBy({
-    top: cardHeight,
+    top: cardHeight * 2,
     behavior: 'smooth',
   });
+}
+
+function onScroll() {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5 && !isLoading) {
+    console.log(pageCount);
+    if (pageCount < totalPages) {
+      onLoadMore();
+    } else {
+      Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  }
 }
